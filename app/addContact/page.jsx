38f1edf55eee04eee39bb22a182d048fd8c50430
justin christensen <justinchristensen@justins-MacBook-Pro-2.local>
2024-01-06 16:47:20 -0500

    added react icons edit and remove. added addContact folder with page.jsx.

---
+++ app/addContact/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="w-[90%] md:w-[30rem] mx-auto my-[5rem] border border-gray-500 bg-white/10 rounded-md p-6">
+      <span className="bg-blue-600/50 p-3 rounded-md hover:bg-blue-700/50 absolute left-[3rem] cursor-pointer">
+        Go Back
+      </span>
+    </div>
+  );
+};
+
+export default page;
--- components/SingleContact.jsx
+++ components/SingleContact.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BsFillTrashFill } from 'react-icons/bs';
+import { AiFillEdit } from 'react-icons/ai';
 
 const SingleContact = ({ item }) => {
   const { fullName, email, phoneNumber, address } = item;
@@ -8,6 +10,14 @@ const SingleContact = ({ item }) => {
       <td>{email}</td>
       <td>{phoneNumber}</td>
       <td>{address}</td>
+      <td className="flex items-center gap-4 text-xl mt-[1rem]">
+        <span className="cursor-pointer hover:opacity-60">
+          <BsFillTrashFill />
+        </span>
+        <span className="cursor-pointer hover:opacity-60">
+          <AiFillEdit />
+        </span>
+      </td>
     </tr>
   );
 };
