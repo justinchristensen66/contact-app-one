323585434595b597693531af346ac6b747775275
justin christensen <justinchristensen@justins-MacBook-Pro-2.local>
2024-01-09 13:25:43 -0500

    Added router and Go back button, started structure for Contact form

--- app/addContact/page.jsx
+++ app/addContact/page.jsx
@@ -1,11 +1,46 @@
+'use client';
+import Input from '@/components/Input';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+  const router = useRouter();
+  const inputs = [
+    {
+      label: 'Full Name',
+      type: 'text',
+      name: 'fullName',
+    },
+    {
+      label: 'Email',
+      type: 'email',
+      name: 'email',
+    },
+    {
+      label: 'Phone No',
+      type: 'text',
+      name: 'phoneNumber',
+    },
+    {
+      label: 'Address',
+      type: 'text',
+      name: 'address',
+    },
+  ];
   return (
     <div className="w-[90%] md:w-[30rem] mx-auto my-[5rem] border border-gray-500 bg-white/10 rounded-md p-6">
-      <span className="bg-blue-600/50 p-3 rounded-md hover:bg-blue-700/50 absolute left-[3rem] cursor-pointer">
+      <span
+        onClick={() => router.push('/')}
+        className="bg-blue-600/50 p-3 rounded-md hover:bg-blue-700/50 absolute left-[3rem] cursor-pointer"
+      >
         Go Back
       </span>
+      <h2 className="pb-7 text-center text-2x1">Contact Form</h2>
+      <form>
+        {inputs.map((item, i) => (
+          <Input item={item} key={i} />
+        ))}
+      </form>
     </div>
   );
 };
